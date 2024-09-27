6f23cd09932e6a5c9b664a7c3b2d49db00057e39
Fernando P.S.D <it21085444@my.sliit.lk>
2024-09-28 01:59:34 +0530

    Update page.jsx

--- client/src/app/voting/page.jsx
+++ client/src/app/voting/page.jsx
@@ -42,7 +42,7 @@ function VotingPage() {
             borderRadius: "10px",
             padding: "20px",
             marginBottom: "20px",
-            backgroundColor: "#f9f9f9",
+            backgroundColor: "black",
             boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
           }}
         >
